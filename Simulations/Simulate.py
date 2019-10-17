41f6f0188e66f078c5395af51ea707de59b66ec8
ReetBarik <reetbarik94@gmail.com>
2019-10-16 18:03:53 -0700

    New Plots

--- Simulations/Simulate.py
+++ Simulations/Simulate.py
@@ -128,7 +128,9 @@ output['Inactive Foragers'] = output['Colony Size'] - output['Adult Drones'] - o
 variableList = ['Colony Size', 'Total Eggs', 'DD', 'L', 'N', 'P', 'dd', 'l', 'n']
 variable = variableList[0]
 
-ax = output.plot(y = variable, color = 'b', alpha = 0.75)
+output['%Colony'] = output['Colony Size'] / output['Colony Size'] * 100 
+
+ax = output.plot(y = variable, color = 'b', legend = False, alpha = 0.75)
 ax.axvspan(1, 31, facecolor='gray', alpha=0.3)
 ax.axvspan(60, 90, facecolor='gray', alpha=0.3)
 ax.axvspan(121, 151, facecolor='gray', alpha=0.3)
@@ -139,20 +141,22 @@ ax.axvspan(366, 396, facecolor='gray', alpha=0.3)
 ax.axvspan(425, 455, facecolor='gray', alpha=0.3)
 ax.axvspan(486, 516, facecolor='gray', alpha=0.3)
 
-y1 = output['Adult Workers']
-y2 = output['Adult Drones']
-y3 = output['Foragers']
-y4 = output['Inactive Foragers']
 
-x = output.index.values
-y = np.vstack([y1, y2, y3, y4])
-labels = ["Adult Workers", "Adult Drones", "Foragers", "Inactive Foragers"]   
-ax.stackplot(x, y, labels = labels)
-ax.legend(loc='upper left', fontsize = 'xx-small')
-ax.set_ylim(0, 90000)
-plt.title('Colony Size (2001) vs Time')
-plt.savefig('Plots/' + Location + '/OnePlus_Stacked_Colony_Size_2001.png', dpi = 1000)
-plt.close()
+for year in range(2002, 2011):
+    params["SimStart"] = "01/01/" + str(year)
+    params["SimEnd"] = "06/30/" + str(year + 1)
+    
+    vp = VarroaPop(parameters = params, weather_file = "VarroaPy/VarroaPy/files/weather/" + weatherFile + ".wth")
+    output = vp.run_model()
+    output = output.drop(output.index[0])
+    output['Inactive Foragers'] = output['Colony Size'] - output['Adult Drones'] -output['Adult Workers'] - output['Foragers']
+    
+    output.plot(y = variable, ax = ax, color = 'b', legend = False, alpha = 0.75)
+
+#ax.set_ylim(0, 150)
+plt.title(variable + ' vs Time')
+#plt.savefig('Plots/' + Location + '/OnePlus_Stacked_Colony_Size_2001%.png', dpi = 1000)
+#plt.close()
     
     
 obsHist = False
@@ -258,7 +262,9 @@ output['Inactive Foragers'] = output['Colony Size'] - output['Adult Drones'] - o
 variableList = ['Colony Size', 'Total Eggs', 'DD', 'L', 'N', 'P', 'dd', 'l', 'n']
 variable = variableList[0]
 
-ax = output.plot(y = variable, color = 'b', alpha = 0.75)
+output['%Colony'] = output['Colony Size'] / output['Colony Size'] * 100 
+
+ax = output.plot(y = variable, color = 'b', legend = False, alpha = 0.75)
 ax.axvspan(1, 31, facecolor='gray', alpha=0.3)
 ax.axvspan(60, 90, facecolor='gray', alpha=0.3)
 ax.axvspan(121, 151, facecolor='gray', alpha=0.3)
@@ -269,16 +275,21 @@ ax.axvspan(366, 396, facecolor='gray', alpha=0.3)
 ax.axvspan(425, 455, facecolor='gray', alpha=0.3)
 ax.axvspan(486, 516, facecolor='gray', alpha=0.3)
 
-y1 = output['Adult Workers']
-y2 = output['Adult Drones']
-y3 = output['Foragers']
-y4 = output['Inactive Foragers']
 
-x = output.index.values
-y = np.vstack([y1, y2, y3, y4])
-labels = ["Adult Workers", "Adult Drones", "Foragers", "Inactive Foragers"]   
-ax.stackplot(x, y, labels = labels)
-ax.legend(loc='upper left', fontsize = 'x-small')
-ax.set_ylim(0, 90000)
-plt.title('Colony Size (2090) vs Time')
-plt.savefig('Plots/' + Location + '/OnePlus_Stacked_Colony_Size_2090.png', dpi = 1000)
+for year in range(2090, 2100):
+    params["SimStart"] = "01/01/" + str(year)
+    params["SimEnd"] = "06/30/" + str(year + 1)
+    
+    vp = VarroaPop(parameters = params, weather_file = "VarroaPy/VarroaPy/files/weather/" + weatherFile + ".wth")
+    output = vp.run_model()
+    output = output.drop(output.index[0])
+    output['Inactive Foragers'] = output['Colony Size'] - output['Adult Drones'] -output['Adult Workers'] - output['Foragers']
+    
+    output.plot(y = variable, ax = ax, color = 'b', legend = False, alpha = 0.75)
+
+#ax.set_ylim(0, 150)
+plt.title(variable + ' vs Time')
+#plt.savefig('Plots/' + Location + '/OnePlus_Stacked_Colony_Size_2001%.png', dpi = 1000)
+#plt.close()
+plt.savefig('Plots/' + Location + '/DD_ObsHist_OnePlus_Overlay_Superimposed_2001_2092.png', dpi = 1000)
+plt.close()
--- Simulations/plot.py
+++ Simulations/plot.py
@@ -1,13 +1,13 @@
-#ax = output.plot(y = variable, color = 'b', legend = False, alpha = 0.75)
-#ax.axvspan(1, 31, facecolor='gray', alpha=0.3)
-#ax.axvspan(60, 90, facecolor='gray', alpha=0.3)
-#ax.axvspan(121, 151, facecolor='gray', alpha=0.3)
-#ax.axvspan(182, 212, facecolor='gray', alpha=0.3)
-#ax.axvspan(244, 273, facecolor='gray', alpha=0.3)
-#ax.axvspan(305, 334, facecolor='gray', alpha=0.3)
-#ax.axvspan(366, 396, facecolor='gray', alpha=0.3)
-#ax.axvspan(425, 455, facecolor='gray', alpha=0.3)
-#ax.axvspan(486, 516, facecolor='gray', alpha=0.3)
+ax = output.plot(y = 'Colony Size', color = 'b', legend = False, alpha = 0.75)
+ax.axvspan(1, 31, facecolor='gray', alpha=0.3)
+ax.axvspan(60, 90, facecolor='gray', alpha=0.3)
+ax.axvspan(121, 151, facecolor='gray', alpha=0.3)
+ax.axvspan(182, 212, facecolor='gray', alpha=0.3)
+ax.axvspan(244, 273, facecolor='gray', alpha=0.3)
+ax.axvspan(305, 334, facecolor='gray', alpha=0.3)
+ax.axvspan(366, 396, facecolor='gray', alpha=0.3)
+ax.axvspan(425, 455, facecolor='gray', alpha=0.3)
+ax.axvspan(486, 516, facecolor='gray', alpha=0.3)
 #
 #
 #for year in range(2002, 2011):
@@ -23,16 +23,16 @@
 
 
 #
-#y1 = output['Adult Workers']
-#y2 = output['Adult Drones']
-#y3 = output['Foragers']
-#y4 = output['Inactive Foragers']
-#
-#x = output.index.values
-#y = np.vstack([y1, y2, y3, y4])
-#labels = ["Adult Workers", "Adult Drones", "Foragers", "Inactive Foragers"]   
-#ax.stackplot(x, y, labels = labels)
-#ax.legend(loc='upper left', fontsize = 'x-small')
-#ax.set_ylim(0, 90000)
-#plt.title('Colony Size (2090) vs Time')
-#plt.savefig('Plots/' + Location + '/OnePlus_Stacked_Colony_Size_2090.png', dpi = 1000)
+y1 = output['Adult Workers']
+y2 = output['Adult Drones']
+y3 = output['Foragers']
+y4 = output['Inactive Foragers']
+
+x = output.index.values
+y = np.vstack([y1, y2, y3, y4])
+labels = ["Adult Workers", "Adult Drones", "Foragers", "Inactive Foragers"]   
+ax.stackplot(x, y, labels = labels)
+ax.legend(loc='upper left', fontsize = 'x-small')
+ax.set_ylim(0, 140000)
+plt.title('Colony Size (2090) vs Time')
+plt.savefig('Plots/' + Location + '/OnePlus_Stacked_Colony_Size_2090.png', dpi = 1000)
