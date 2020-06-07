35beed0788bd23552fe4401115332a990d9bfff8
Julien Pireaud <jpireaud@gmail.com>
2020-06-06 23:56:21 -0700

    - Finished renaming of VarroaPop.* files - fixed setup.py file to include portcode files in the lowerification of cases of include files from the data model

---
+++ Linux/portcode/varroapop.cpp
@@ -0,0 +1,15 @@
+#include "varroapop.h"
+
+bool gl_RunGUI = false;
+
+int MyMessageBox( LPCTSTR lpszText, UINT nType, UINT nIDHelp )
+{
+    NOT_IMPLEMENTED();
+    return -1;
+}
+
+CString SplitPath(CString PathString, PELEMENT PathElement)
+{
+    NOT_IMPLEMENTED();
+    return CString();
+}
