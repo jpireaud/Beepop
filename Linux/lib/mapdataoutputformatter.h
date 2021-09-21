ec0812c47702d4fde71a612ed0ede310489c7ebf
Julien Pireaud <jpireaud@gmail.com>
2021-09-21 10:25:58 -0700

    update mapdataoutput to include days bellow 7500 - 10000 and dead foragers over winter

--- Linux/lib/mapdataoutputformatter.h
+++ Linux/lib/mapdataoutputformatter.h
@@ -2,8 +2,9 @@
 
 #include "customoutputformatter.h"
 
-#include <sstream>
 #include <limits>
+#include <queue>
+#include <sstream>
 
 /**
  * Output formatter that will only output the colony size
@@ -11,22 +12,28 @@
 class MapDataOutputFormatter : public CustomOutputFormatter
 {
 public:
-    MapDataOutputFormatter(CVarroaPopSession& session);
+	MapDataOutputFormatter(CVarroaPopSession& session);
 
 	virtual ~MapDataOutputFormatter();
 
 	virtual void Init(CColony& colony);
 
 	virtual void Record(CColony& colony, CEvent& event);
-    
+
 protected:
-    
-    struct MapDataItem
-    {
-        MapDataItem() {}
-        
-        int m_daysBellow5000 = 0;
-        int m_minColonySize = std::numeric_limits<int>::max();
-    };
-    MapDataItem m_item;
+	struct MapDataItem
+	{
+		MapDataItem() {}
+
+		int m_daysBellow7500 = 0;
+		int m_daysBellow10000 = 0;
+		int m_minColonySize = std::numeric_limits<int>::max();
+	};
+	MapDataItem m_item;
+
+	// here we need this attribute outside the MapDataItem since it's value
+	// is updated accross years
+	bool            m_recordDeadForagers = false;
+	int             m_deadForagersOverWinter = 0;
+	std::queue<int> m_deadForagersOverWinterCounts;
 };
